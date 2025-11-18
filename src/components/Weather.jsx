c2271d4d63736995566e42b88cb8dff6637b3270
Sambhav Goel <sambhavgoel123@gmail.com>
2025-11-18 15:35:29 +0530

    updated something

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -22,7 +22,12 @@ const Weather = () => {
             const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
             const response = await fetch(url);
-            const data = await response.json(); 
+            const data = await response.json();
+            if(!response.ok){
+              alert(data.message);
+              return;
+            }
+            
             setWeatherData({
                 humidity : data.main.humidity,
                 windSpeed : data.wind.speed,
@@ -33,6 +38,11 @@ const Weather = () => {
             
         }
     }
+
+    useEffect(()=>{
+      search("Delhi");
+    } ,[])
+
   return (
     <div className='weather'>
       <div className="search-bar">
